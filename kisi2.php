12971efc74e23bc6b70f69179f7614191907bbff
naf05 <nafisahzahrotun005@gmail.com>
2023-03-07 11:55:55 +0700

    tugas php

---
+++ kisi2.php
@@ -0,0 +1,15 @@
+<?php
+
+$nasabah = "Kelas2";
+
+if ($nasabah == "VIP") {
+    echo  "kartu atm,buku tabungan,M-Banking,menjadi prioritas";
+}
+ else if ($nasabah == "Kelas1") {
+    echo  "kartu atm,M-Banking,buku tabungan";
+}
+ else if ($nasabah == "Kelas2") {
+    echo  "kartu atm,buku tabungan";
+} 
+?>
+
---
+++ kisi5.php
@@ -0,0 +1,8 @@
+<?php
+
+$perusahaan = ["Toyota", "Daihatsu", "Honda", "Ford", "Ferari"];
+echo "Perusahaan "  .$perusahaan  [4];
+echo "<br>";
+echo "Perusahaan "  .$perusahaan  [1] ." dan " .$perusahaan  [2];
+
+?>
